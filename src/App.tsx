752162888fc0f097766f507aabd1fcea0090949e
Sympall <drkknit@gmail.com>
2021-08-28 01:35:24 +0530

    router integrate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import "./App.css";
+import { EditScreen, Streams } from "./components/page";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/edit/:id">
+            <EditScreen title={"Edit stream"} />
+          </Route>
+          <Route path="/edit">
+            <EditScreen title={"Create a stream"} />
+          </Route>
+          <Route path="/">
+            <Streams />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
