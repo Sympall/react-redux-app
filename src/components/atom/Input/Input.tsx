8fb6f55ae8a9e8d52cb1914296e26237019080b2
Sympall <drkknit@gmail.com>
2021-08-28 01:36:13 +0530

    default value when updating old list

--- src/components/atom/Input/Input.tsx
+++ src/components/atom/Input/Input.tsx
@@ -3,13 +3,22 @@ import "./Input.scss";
 
 interface InputProps {
   label: string;
+  defaultValue?: string;
 }
 
-const Input = ({ label }: InputProps, ref: ForwardedRef<HTMLInputElement>) => {
+const Input = (
+  { label, defaultValue = "" }: InputProps,
+  ref: ForwardedRef<HTMLInputElement>
+) => {
   return (
     <div className="input-wrapper">
       <div className="label">{label}</div>
-      <input ref={ref} type="text" className="input-field" />
+      <input
+        ref={ref}
+        type="text"
+        className="input-field"
+        defaultValue={defaultValue}
+      />
     </div>
   );
 };
